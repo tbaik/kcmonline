fdafe79de6ef2b9be3d14c36cd6019e7fc52141a
shyunz <baiktony@yahoo.com>
2013-04-25 03:22:02 -0700

    host fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ KcmOnline::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'kcmonline.org' }
+  config.action_mailer.default_url_options = { :host => 'kcmfinance.herokuapp.com' }
 
   #config.action_mailer.default_url_options = { :host => 'kcmonline.org' }
 
