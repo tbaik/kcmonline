911b6cc9e5ac86a9f562a21b93675887a2aa3237
shyunz <baiktony@yahoo.com>
2013-05-13 01:08:28 -0700

    testing2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,15 @@ KcmOnline::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'kcmfinance.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "587",
     :authentication => :plain,
     :user_name      => ENV['app14182010@heroku.com'],
     :password       => ENV['zjmeksgg'],
-    :domain         => ENV['kcmfinance.herokuapp.com']
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   } 
 
   #config.action_mailer.default_url_options = { :host => 'kcmonline.org' }
